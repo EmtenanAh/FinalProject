145d1e93194d58952a92d63a927d8f6b8cfdaf82
emtenan ahmed <emtenan_001@hotmail.com>
2021-12-08 15:40:51 +0300

    splash Activity

---
+++ app/src/main/java/com/example/finalproject/View/Registration.kt
@@ -0,0 +1,12 @@
+package com.example.finalproject.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finalproject.R
+
+class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
