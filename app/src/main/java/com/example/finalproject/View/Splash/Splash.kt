95d9ada6d80115cc7132d5e322da4a73d74c08da
emtenan ahmed <emtenan_001@hotmail.com>
2021-12-30 15:33:33 +0300

    NotifictionActivity

--- app/src/main/java/com/example/finalproject/View/Splash/Splash.kt
+++ app/src/main/java/com/example/finalproject/View/Splash/Splash.kt
@@ -8,21 +8,25 @@ import android.view.animation.AnimationUtils
 import android.widget.TextView
 import com.example.finalproject.R
 import com.example.finalproject.View.Login.Login
+import com.google.firebase.messaging.FirebaseMessaging
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        var tooAnimation = AnimationUtils.loadAnimation(this,R.anim.toop)
-        var   txt_Kindergarten = findViewById<TextView>(R.id.txt_Kindergarten)
+        var tooAnimation = AnimationUtils.loadAnimation(this, R.anim.toop)
+        var txt_Kindergarten = findViewById<TextView>(R.id.txt_Kindergarten)
         txt_Kindergarten.startAnimation(tooAnimation)
 
-        val splashscteen=5000
-        val intent = Intent(this, Login::class.java)
-        Handler().postDelayed({
-            startActivity(intent)
-            finish()
-        },splashscteen.toLong())
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            var token = it.result.toString()
+            print(token)
+            val splashscteen = 5000
+            val intent = Intent(this, Login::class.java)
+            Handler().postDelayed({
+                startActivity(intent)
+                finish()
+            }, splashscteen.toLong())
+        }
+}}
 
-    }
-}
